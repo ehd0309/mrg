9546d036ea420432454c9a68673c2486917f1eae
ehd0309 <ehd0309@cryptolab.co.kr>
2024-11-02 22:43:58 +0900

    chore(app-chat): init

---
+++ app-chat/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Button color="primary">HELO</Button>
+      HELLO WORLD
+    </div>
+  );
+}
